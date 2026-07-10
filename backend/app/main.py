b76cd460bc68e5c11552ac395283e88658078035
aashirvad08 <148707922+aashirvad08@users.noreply.github.com>
2026-07-10 22:31:31 +0530

    cors settings update

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # In production specify the Next.js URL
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
